da102bef32798edd14d8faab4eb5791d0b69b35a
Rafael Pelle <rafapelle@gmail.com>
2019-08-31 14:38:01 -0300

    Implements search results filtered by year AND searchTerm (title, author and isbn).

--- src/services/books/controllers.ts
+++ src/services/books/controllers.ts
@@ -2,15 +2,33 @@ import { Request, Response, NextFunction } from 'express'
 import { Book } from '../../models/book'
 
 export const getBooks = async (req: Request, res: Response, next: NextFunction) => {
-	const { initialDate, endDate } = req.query
+	const { initialDate, endDate, searchTerm } = req.query
+	let books = []
 
-	const books =
-		!initialDate && !endDate
-			? await Book.query()
-			: await Book.query().whereBetween('year', [initialDate, endDate])
+	if (searchTerm) {
+		const subquery = Book.query()
+			.orWhere('title', 'like', `%${searchTerm}%`)
+			.orWhere('author', 'like', `%${searchTerm}%`)
+			.orWhere('isbn', 'like', `%${searchTerm}%`)
+			.select('id')
+		books =
+			!initialDate && !endDate
+				? await Book.query().where('id', 'in', subquery)
+				: await Book.query()
+						.whereBetween('year', [initialDate, endDate])
+						.andWhere('id', 'in', subquery)
+	} else {
+		books =
+			!initialDate && !endDate
+				? await Book.query()
+				: await Book.query().whereBetween('year', [initialDate, endDate])
+	}
 
 	res.status(200).send({
 		ok: true,
 		books,
+		initialDate,
+		endDate,
+		searchTerm,
 	})
 }
